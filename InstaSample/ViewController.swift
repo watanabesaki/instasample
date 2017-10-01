80060ffbd445c7d0ebbcc62563ed08073146d9db
渡辺早紀 <watanabesaki@watanabesakinoMacBook.local>
2017-10-01 12:18:38 +0900

    Initial Commit

---
+++ InstaSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaSample
+//
+//  Created by 渡辺早紀 on 2017/10/01.
+//  Copyright © 2017年 Saki Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
